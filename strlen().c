91bda6baa5f358ffc2170a96c70f2a32c0254326
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-18 20:38:54 +0100

    .

---
+++ strlen().c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    char str[] = "Hola mundo";
+    int length = strlen(str);
+    printf("La longitud de '%s' es: %d\n", str, length);
+
+    return 0;
+}
